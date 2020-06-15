57bfd1a88c95425375e3a3950d7316cde46cc35a
Dawid-Olcha <45019768+Dawid-Olcha@users.noreply.github.com>
2020-06-15 16:25:05 +0200

    Update KartaPacjenta.cs

--- cSharp_1/KartaPacjenta.cs
+++ cSharp_1/KartaPacjenta.cs
@@ -8,15 +8,33 @@ namespace cSharp_1
     public class KartaPacjenta
     {
 
-        public List<string> przebyteChoroby = new List<string>(); // w wywiadzie?
+        public string przebiegChoroby;
         public Wywiad? wywiad;
         public Wypis? wypis;   
         public string Grupa_Krwi { get; set; }
+        public Date dataPrzyjecia;
+        public Date? dataWypisu;
+        public int telOsobyKontaktowej;
 
-        KartaPacjenta()
+        KartaPacjenta(string przebieg, string krew, int tel)
         {
-   
+            przebiegChoroby = przebieg;
+            Grupa_Krwi = krew;
+            dataPrzyjecia = Date.Now;
+            telOsobyKontaktowej = tel;
+            wywiad = null;
+            wypis = null;
+            dataWypisu = null;
         }
+        
+        edytujKartaPacjenta(int string przebieg, string krew, int tel)
+        {
+            przebiegChoroby = przebieg;
+            Grupa_Krwi = krew;
+            telOsobyKontaktowej = tel; 
+        }
+        
+        
 
     }
 }
