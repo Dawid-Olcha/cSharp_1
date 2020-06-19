adc6d649e09e9f37834117394b5968ba4f7ec9ae
Dawid-Olcha <45019768+Dawid-Olcha@users.noreply.github.com>
2020-06-19 12:40:11 +0200

    Update Wywiad.cs

--- cSharp_1/Wywiad.cs
+++ cSharp_1/Wywiad.cs
@@ -6,11 +6,16 @@ namespace cSharp_1
 {
     public class Wywiad
     {
-        public List<string> przebyteChoroby = new List<string>(); 
-        public List<string> rodzinaChoroby = new List<string>(); 
-        
-    }
-    
+        public List<string> przebyteChoroby; 
+        public List<string> rodzinaChoroby; 
+
+     public Wywiad()
+     {
+        przebyteChoroby = new List<string>(); 
+        rodzinaChoroby = new List<string>();   
+     }
+
+
     public void dodajPrzebytaChoroba(string pCh)
     {
         przebyteChoroby.Add(pCh);
@@ -20,6 +25,11 @@ namespace cSharp_1
     {
         rodzinaChoroby.Add(rCh);
     }
+
+        
+    }
+    
+
     
     
 }
