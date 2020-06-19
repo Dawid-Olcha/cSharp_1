471c58bd742666eb9a7f21f39f7a9a7a374195fd
Dawid-Olcha <45019768+Dawid-Olcha@users.noreply.github.com>
2020-06-19 11:09:51 +0200

    Create Choroba.cs

---
+++ cSharp_1/Choroba.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace cSharp_1
+{
+    public class Choroba
+    {
+        public string NazwaChoroby{get; set;}
+
+  
+        public Choroba( string nazwa)
+        { 
+            NazwaChoroby = nazwa;
+        }
+          
+ 
+    
+    
+   } 
+}
